d3660ee7baccc732e38fc71cabf66ba4908e6f04
Amos Wenger <amos@official.fm>
2011-10-26 14:11:52 +0200

    Added clamp to fragment shader

--- src/exercises/01-CubeViewer/cube.fs
+++ src/exercises/01-CubeViewer/cube.fs
@@ -5,7 +5,7 @@ varying vec4 color;
 void main()
 {
 	// Exercise 3.3.2
-    gl_FragColor = dot(normalize(normal), normalize(vertex_to_light_vector)) * color;
+    gl_FragColor = clamp(dot(normalize(normal), normalize(vertex_to_light_vector)) * color, 0.0, 1.0);
 	
 	// need this line so OpenGL doesn't optimize out the variables -- remove in your solution
     // {
